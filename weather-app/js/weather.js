507646663f50c675bf8d616b3cd2c74052a7d895
Gabriel Domingues Manoel <78428088+LordMan0@users.noreply.github.com>
2021-03-15 17:32:02 -0300

    Add files via upload

---
+++ weather-app/js/weather.js
@@ -0,0 +1,25 @@
+const APIKey = 'VPZ4OWM3TtQBNd4CTQw9SKntb2Ge83w4'
+const baseUrl = 'https://dataservice.accuweather.com/'
+
+const getCityInfoUrl = cityName => 
+    `${baseUrl}locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getWeatherInfoUrl = cityKey => 
+    `${baseUrl}currentconditions/v1/${cityKey}?apikey=${APIKey}&language=pt-br` 
+
+const getFetchData = async url => {
+    try {
+        const response = await fetch(url)
+
+        if (!response.ok){
+            throw new Error('Não foi possível obter os dados da requisição.')
+        }
+
+        return response.json()
+    } catch ({ name, message }) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+const getCityInfo = cityName => getFetchData(getCityInfoUrl(cityName))
+const getWeatherInfo = cityKey => getFetchData(getWeatherInfoUrl(cityKey))
